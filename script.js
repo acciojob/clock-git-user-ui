fe0a2455f9c2bdcc26afb653b4780da126a5e209
git-user-ui <pratikpp2302@gmail.com>
2025-12-02 14:13:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const pTag = document.getElementById("timer");
+function currentDateTime(){
+	
+    const now = new Date();
+
+    // Format date
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0"); // Months start at 0
+    const year = now.getFullYear();
+
+    let hours = String(now.getHours()).padStart(2, "0");
+	const minutes = String(now.getMinutes()).padStart(2, "0");
+	const seconds = String(now.getSeconds()).padStart(2, "0");
+    const ampm = hours >= 12 ? "PM" : "AM";
+
+	hours = hours % 12;
+    hours = hours ? hours : 12; // 0 becomes 12
+    hours = String(hours).padStart(2, "0");
+	
+    const finalDateTime = `${month}/${day}/${year},  ${hours}:${minutes}:${seconds} ${ampm}` 
+
+ pTag.innerHTML = finalDateTime;
+}
+
+currentDateTime()
+setInterval(currentDateTime, 1000)
